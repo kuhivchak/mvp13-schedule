ba8953f8be0137832d375e147466acae2b01ddea
kuhivchak <vkuhi@softserveinc.com>
2020-02-09 09:59:32 +0200

    Added dynamic green color for numbers.

--- schedule-animation.js
+++ schedule-animation.js
@@ -4,13 +4,18 @@ animation();
 		var eightFebruaryDate = new Date("2/9/2020");
 		var numberDiff = daysDiff(eightFebruaryDate, currentDate);
 		var denominator = (((numberDiff / 7) % 2) < 1);
-		var dynamicElements = denominator ? document.getElementsByClassName("denominator") : document.getElementsByClassName("numerator");
-		setBackgroudColor(dynamicElements, "lightgreen");
-		var staticElements = document.getElementsByClassName("container");
-		setBackgroudColor(staticElements, "lightgreen");
+		var dynamicSubjectElements = denominator ? document.getElementsByClassName("denominator") : document.getElementsByClassName("numerator");
+		setBackgroudColor(dynamicSubjectElements, "lightgreen");
+		var staticSubjectElements = document.getElementsByClassName("container");
+		setBackgroudColor(staticSubjectElements, "lightgreen");
+		var staticNumberBlocks = document.getElementsByClassName("static number-block");
+		setBackgroudColor(staticNumberBlocks, "lightgreen");
+		var dynamicNumberBlocks = getNonEmptyNumberBlocks(dynamicSubjectElements);
+		setBackgroudColor(dynamicNumberBlocks, "lightgreen");
 		if (studyDay(currentDate)) {
-			var dayElement = document.getElementsByClassName(getCurrentWeekDayInFormat('long').toLowerCase());
-			setBackgroudColor(dayElement, "lightgreen");
+			var todayDay = getCurrentWeekDayInFormat('long').toLowerCase();
+			var dayBlockElement = document.getElementsByClassName(todayDay + " day-row");
+			setBackgroudColor(dayBlockElement, "lightgreen");
 		}
 	}
 
@@ -18,6 +23,17 @@ animation();
 		dt1 = new Date(date1);
 		dt2 = new Date(date2);
 		return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+	}
+
+	function getNonEmptyNumberBlocks(dynamicSubjectElements) {
+		var dynamicNumberBlocks = document.getElementsByClassName("dynamic number-block");
+		var nonEmptyNumberBlocks = [];
+		for (var i = 0; i < dynamicSubjectElements.length; i++) {
+	   		if (!(dynamicSubjectElements[i].classList.contains("empty"))) {
+	   			nonEmptyNumberBlocks.push(dynamicNumberBlocks[i]);
+	   		}
+	 	 }
+	 	 return nonEmptyNumberBlocks;
 	}
 
 	function setBackgroudColor(elementByClass, color) {
